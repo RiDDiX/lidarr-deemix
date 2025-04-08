f4cd0413cb771fb03667851f2ac81a385fb5702b
RiDDiX <github@egu-rel.de>
2025-04-08 22:17:50 +0200

    Try fixing this 3000

--- python/http-redirect-request.py
+++ python/http-redirect-request.py
@@ -1,15 +1,18 @@
 from mitmproxy import http
 
 def request(flow: http.HTTPFlow) -> None:
+    # Debug-Ausgabe
+    print("Incoming request:", flow.request.pretty_host, flow.request.host, flow.request.port)
+    
     # Sonderfall: Spotify nicht umleiten
     if flow.request.pretty_host == "https://api.lidarr.audio/api/v0.4/spotify/":
         return
     
-    # Prüfe, ob der Request entweder an "api.lidarr.audio",
-    # "ws.audioscrobbler.com" oder an "localhost" (mit Port 3000) erfolgt.
+    # Prüfe, ob der Request an "api.lidarr.audio", "ws.audioscrobbler.com" oder "localhost:3000" erfolgt.
     if (flow.request.pretty_host in ["api.lidarr.audio", "ws.audioscrobbler.com"]) \
        or (flow.request.host == "localhost" and flow.request.port == 3000) \
        or ("localhost:3000" in flow.request.pretty_host):
+        print("Redirecting request from", flow.request.pretty_host)
         flow.request.headers["X-Proxy-Host"] = flow.request.pretty_host
         flow.request.scheme = "http"
         flow.request.host = "127.0.0.1"
