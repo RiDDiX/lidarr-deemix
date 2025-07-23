ef097791c5812486628b5320f02517f5630414ec
RiDDiX <github@egu-rel.de>
2025-07-23 14:21:36 +0200

    I am stupid

--- python/http-redirect-request.py
+++ python/http-redirect-request.py
@@ -4,4 +4,4 @@ def request(flow: http.HTTPFlow) -> None:
     if flow.request.pretty_host == "api.lidarr.audio":
         flow.request.scheme = "http"
         flow.request.host   = "127.0.0.1"
-        flow.request.port   = 8080
+        flow.request.port   = 7171
